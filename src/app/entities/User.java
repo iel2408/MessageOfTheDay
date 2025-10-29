d759da019c9d4988f42673126396420cc284d457
iel2408 <161427873+iel2408@users.noreply.github.com>
2025-10-29 13:51:25 +0800

    Update User.java

--- src/app/entities/User.java
+++ src/app/entities/User.java
@@ -1,1 +1,46 @@
-test
+package app.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	@Column
+	private Long pk;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String cellphoneNumber;
+
+	public Long getPk() {
+		return pk;
+	}
+
+	public void setPk(Long pk) {
+		this.pk = pk;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public String setName(String name) {
+		this.name = name;
+	}
+
+	public String getCellphoneNumber() {
+		return cellphoneNumber;
+	}
+
+	public void setCellphoneNUmber(String cellphoneNumber) {
+		this.cellphoneNumber = cellphoneNumber;
+	}
+}
