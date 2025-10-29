670a1db72702d2af2b447bd6c108ce3785232e2d
iel2408 <161427873+iel2408@users.noreply.github.com>
2025-10-29 13:56:54 +0800

    Update UserRepository.java

--- src/app/repositories/UserRepository.java
+++ src/app/repositories/UserRepository.java
@@ -1,1 +1,16 @@
-a
+package app.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import app.entities.User;
+
+@Repository
+public interface UserRepository 
+	extends JpaRepository<User, Long> 
+{
+	public List<User> findAll();
+	public User findByPk(Long pk);
+}
