6ad14eb39ae3066144297a1b1d2d2dbd33bb156c
iel2408 <161427873+iel2408@users.noreply.github.com>
2025-10-29 14:07:58 +0800

    Update UserInitializer.java

--- src/app/components/UserInitializer.java
+++ src/app/components/UserInitializer.java
@@ -1,1 +1,42 @@
-a
+package app.components;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import app.entities.User;
+import app.repositories.UserRepository;
+
+@Component
+public class UserInitializer {
+	@Autowired
+	private UserRepository repo;
+	
+	@PostConstruct 
+	public void init() {
+		if (repo.count()==0) {
+			User user = makeUser("Gab", "09953034712");
+			repo.save(user);
+
+			user = makeUser("Acia", "09953034712");
+			repo.save(user);
+
+			user = makeUser("Evan", "09953034712");
+			repo.save(user);
+
+			user = makeUser("Seppi", "09953034712");
+			repo.save(user);
+
+			user = makeUser("EJ", "09953034712");
+			repo.save(user);
+		}
+	}
+	
+	public Quote makeUser(String name, String cellphoneNumber) {
+		User u = new User();
+		u.setName(name);
+		u.setCellphoneNumber(cellphoneNumber)
+		return u;
+	}
+}
